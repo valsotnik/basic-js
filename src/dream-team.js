012aea969cebc04fb3d52895fa578af72a34a0ad
Val Sotnik <cmvaltime@gmail.com>
2022-03-13 22:53:57 +0300

    feat: dream team task complete

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+	if (Array.isArray(members) !== true) return false
+  return members.filter(a => typeof a === "string")
+								.map(a => a.replace(/\s/g, '').slice(0,1).toUpperCase())
+								.sort()
+								.join('')
 }
 
 module.exports = {
